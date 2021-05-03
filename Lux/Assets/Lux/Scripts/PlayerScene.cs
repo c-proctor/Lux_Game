c820e6cd7c042526a424d6b1844059055057ed40
c-proctor <79915136+c-proctor@users.noreply.github.com>
2021-05-03 12:05:46 +0800

    menu ui and prefab updates

--- Lux/Assets/Lux/Scripts/PlayerScene.cs
+++ Lux/Assets/Lux/Scripts/PlayerScene.cs
@@ -22,17 +22,17 @@ public class PlayerScene : MonoBehaviour
         if(col.gameObject.CompareTag("FireDoor"))
         {
             Debug.Log("FIREWORLD");
-            SceneManager.LoadScene(2);
+            SceneManager.LoadScene(3);
         }
         if(col.gameObject.CompareTag("IceDoor"))
         {
             Debug.Log("ICEWORLD");
-            SceneManager.LoadScene(1);
+            SceneManager.LoadScene(2);
         }
         if(col.gameObject.CompareTag("Exit"))
         {
             Debug.Log("EXIT");
-            SceneManager.LoadScene(0);
+            SceneManager.LoadScene(1);
         }
        
     }
