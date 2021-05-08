e4c64451e3610f60b211e752809cc2f4874aaef3
c-proctor <79915136+c-proctor@users.noreply.github.com>
2021-05-08 21:03:15 +0800

    quest system
    questing system added. multiple compile errors related to player (further work required)

---
+++ Lux/Assets/Quest System Test/Scripts/QuestGiver.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestGiver : MonoBehaviour
+{
+   public Quest quest;
+   
+   public Player player;
+
+    public GameObject questWindow;
+    // public Text titleText;
+    // public Text descriptionText;
+
+   public void OpenQuestWindow()
+   {
+       questWindow.SetActive(true);
+       // titleText.text = quest.title;
+       // descriptionText.text = quest.description;
+   }
+
+   public void AcceptQuest()
+   {
+       questWindow.SetActive(false);
+       quest.isActive = true;
+       // give to player
+       player.quest = quest;
+   }
+}
