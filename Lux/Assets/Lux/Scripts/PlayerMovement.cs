d51eaf4414ab52d0cebf7cace88d422b3409d481
Casey <c.proctor527@gmail.com>
2021-05-10 14:50:13 +0800

    ice works!!!! :))))))))))))))))))))))))

--- Lux/Assets/Lux/Scripts/PlayerMovement.cs
+++ Lux/Assets/Lux/Scripts/PlayerMovement.cs
@@ -9,22 +9,96 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     Vector2 movement;
+    Vector2 input;
+    bool isNormalMovement = true; //false would be skating
+    bool justCollidedWithIceBlock = false; //only allow skate input after a block collision
    
 
     // Update is called once per frame
     void Update()
     {
         //Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        
 
+        /*
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+        */
+        if (isNormalMovement)
+        {
+            NormalMovement();
+        }
+        else
+        {
+            SkatingMovement();
+        }
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    void NormalMovement()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.y);
+        animator.SetFloat("Speed", input.sqrMagnitude);
+        movement = input;
+    }
+
+    void SkatingMovement()
+    {
+        if (justCollidedWithIceBlock)
+        {
+            //Debug.Log("Collecting skating movement");
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+            
+            //only clear the justcollided flag if the player pressed something
+            if (input.magnitude > 0.01f)
+            {
+                movement = input;
+                justCollidedWithIceBlock = false;
+            }
+        }
+        
+        animator.SetFloat("Horizontal", input.x);
+        animator.SetFloat("Vertical", input.y);
+        animator.SetFloat("Speed", input.sqrMagnitude);
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "IceFloor")
+        {
+            isNormalMovement = false;
+            justCollidedWithIceBlock = false;
+            //movement = Vector2.zero;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "IceFloor")
+        {
+            isNormalMovement = true;
+            //movement = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        Collider2D col = collision.collider;
+        Debug.Log("Collided with " + col.tag);
+        if (col.tag == "IceBlock")
+        {
+            justCollidedWithIceBlock = true;
+            //movement = Vector2.zero;
+        }
+    }
+
 }
--- Lux/Assets/Quest System Test/Scripts/QuestGiver.cs
+++ Lux/Assets/Quest System Test/Scripts/QuestGiver.cs
@@ -1,22 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestGiver : MonoBehaviour
 {
    public Quest quest;
    
-   public Player player;
+   public TestPlayer player;
 
     public GameObject questWindow;
-    // public Text titleText;
-    // public Text descriptionText;
+    public Text titleText;
+    public Text descriptionText;
 
    public void OpenQuestWindow()
    {
        questWindow.SetActive(true);
-       // titleText.text = quest.title;
-       // descriptionText.text = quest.description;
+        titleText.text = quest.title;
+       descriptionText.text = quest.description;
    }
 
    public void AcceptQuest()
@@ -24,6 +25,6 @@ public class QuestGiver : MonoBehaviour
        questWindow.SetActive(false);
        quest.isActive = true;
        // give to player
-       //player.quest = quest;
+       player.quest = quest;
    }
 }
