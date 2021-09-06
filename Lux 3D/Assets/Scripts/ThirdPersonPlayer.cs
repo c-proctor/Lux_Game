3088607d33a129542a834560a31f2fe4b6c92646
Accident Prone <48279457+Accident-Prone@users.noreply.github.com>
2021-09-06 15:10:36 +0800

    Fixed up player movement
    There is now controller support as well as being able to move according to direction camera is facing. Also put a cube on top of the player to tell what direction they are currently facing (replace with player model)

--- Lux 3D/Assets/Scripts/ThirdPersonPlayer.cs
+++ Lux 3D/Assets/Scripts/ThirdPersonPlayer.cs
@@ -21,10 +21,14 @@ public class ThirdPersonPlayer : MonoBehaviour
     PlayerInput input;
 
     public Transform playerCamera;
+    float targetAngle;
+    float angle;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVel;
 
     MovementType moveType = MovementType.Normal;
     private float nextFireTime;
-    public float fireRate;
+    public float fireRate = 0.6f;
 
     // In case we want more movement types
     public enum MovementType
@@ -37,7 +41,7 @@ public class ThirdPersonPlayer : MonoBehaviour
     {
         input = new PlayerInput();
 
-        //Leave this be, if it works, it works
+        //Leave this be, if it works, it works. Gets movement based inputs
         input.Player.Move.performed += ctx =>
         {
             currentMove = ctx.ReadValue<Vector2>();
@@ -55,16 +59,17 @@ public class ThirdPersonPlayer : MonoBehaviour
         
     }
 
-
+    // Shoot projectile (based on fire rate and if holding down shoot button)
     public void ShootProjectile(InputAction.CallbackContext context)
     {
         if(nextFireTime < Time.time && context.performed)
         {
             playerBullet.GetComponent<PlayerBullet>().SwitchType(selectedType);
-            Instantiate(playerBullet, bulletPoint.transform.position, Quaternion.identity);
+            Instantiate(playerBullet, bulletPoint.transform.position, transform.rotation);
             nextFireTime = Time.time + fireRate;
         }
     }
+    // For changing weapon type (can setup as an if check when having actual pickups)
     public void WeaponIce(InputAction.CallbackContext context)
     {
         selectedType = PlayerBullet.BulletType.Ice;
@@ -76,6 +81,7 @@ public class ThirdPersonPlayer : MonoBehaviour
 
     private void HandleRotation()
     {
+        /*
         Vector3 currentPosition = transform.position;
 
         Vector3 newPosition = new Vector3(currentMove.x, 0f, currentMove.y);
@@ -83,31 +89,31 @@ public class ThirdPersonPlayer : MonoBehaviour
         Vector3 lookAtPos = currentPosition + newPosition;
 
         transform.LookAt(lookAtPos);
+        */
+        //transform.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 
     private void HandleMovement()
     {
+        Vector3 direction = new Vector3(currentMove.x, 0f, currentMove.y).normalized;
         //Incorporate with animator from this tutorial (https://www.youtube.com/watch?v=IurqiqduMVQ) at ~ 14.30mins
         if (movePressed)
         {
-            transform.position = transform.position + transform.forward * Time.deltaTime;
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.position += moveDir.normalized * moveSpeed * Time.deltaTime;
         }
     }
 
     private void Update()
     {
-        HandleRotation();
+        //HandleRotation();
         HandleMovement();
     }
 
-    /*
-    public void Move(InputValue input)
-    {
-        Vector3 inputVec = input.Get<Vector3>();
-        Vector3 moveVector = new Vector3(inputVec.x, 0f, inputVec.z);
-        //controller.Move(moveVector * moveSpeed * Time.deltaTime);
-    }
-    */
     private void OnEnable()
     {
         input.Player.Enable();
@@ -127,5 +133,11 @@ public class ThirdPersonPlayer : MonoBehaviour
     {
         playerHealth += addedHealth;
         Debug.Log("Player health added");
+    }
+
+    // Return the player health
+    public int GetHealth()
+    {
+        return playerHealth;
     }
 }
